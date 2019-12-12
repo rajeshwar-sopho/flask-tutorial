b33237d549e5f64f7aa143c9c7671dca4f78477e
rajeshwar-sopho <rajeshwarrn98@gmail.com>
2019-12-12 15:27:05 +0530

    first commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,40 @@
+# contains the application factory
+# tells python to treat this as package
+
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    # instance_relative_config tells that config are relative to the instance
+    app = Flask(__name__, instance_relative_config=True)
+    # it is name of the package
+    print(__name__) # flaskr
+    # 'instance' next to the package or module is assumed to be the instance path.
+    # i.e in this case flaskr is assumed to be the instance path.
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load test_config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensures that instance folder exists as
+    # sqlite file exists at the app instance
+    try:
+        # new instance directory is created for app
+        print(app.instance_path) # F:\btech_books\web_works\flask-tutorial\instance
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # simple application
+    @app.route('/hello')
+    def hello():
+        return 'hello!'
+    
+    return app
